95833bdf38b3c350bba824bd5195742635777322
Olga Yezerska <olga_ezerska06@ukr.net>
2026-02-11 19:44:56 +0200

    Feature: add pause handler in InputHandler

--- classes/input_handler.py
+++ classes/input_handler.py
@@ -1,16 +1,27 @@
 import pygame
 class InputHandler:
+    """
+    Клас для обробки введень користувача
+    """
     def __init__(self, player=None, game=None):
+        """
+        Конструктор класу
+        """
         self.player = player
         self.game = game
 
     def handle_input(self, events: list[pygame.event.Event]) -> dict:
-        result = {}  # словник з діями, які потрібно обробити в Game
-        
+        """
+        Метод обробки отриманих даних
+        Args: 
+           events: події, що надходять
+        """
+        result = {} #список результатів
+
         for event in events:
             if event.type == pygame.QUIT:
                 result['quit'] = True
-                return result  # можна одразу вийти
+                return result  
             
         keys = pygame.key.get_pressed()
         
@@ -20,5 +31,12 @@ class InputHandler:
 
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
                 self.player.move_right()
+
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE or event.key == pygame.K_p:
+                    if self.game is not None:
+                        self.game.is_paused = not self.game.is_paused
+                    result['pause_toggle'] = True
 
         return result
