80a63ed3d43dde95512c57793589631e8f322091
Nazar2903 <2222mnazar2222@gmail.com>
2026-02-15 14:26:56 +0200

    docs: add CollisionSystem class description

--- classes/collision_system.py
+++ classes/collision_system.py
@@ -3,10 +3,21 @@ from classes.interfaces import ICollidable
 
 class CollisionSystem:
     def __init__(self, player: ICollidable, obstacle_manager):
+        """
+        Initializes the collision system with the player and obstacle manager.
+        
+        :param player: The player's car object that implements ICollidable.
+        :param obstacle_manager: The manager handling obstacles.
+        """
         self.player = player
         self.obstacle_manager = obstacle_manager
 
     def check_collisions(self) -> bool:
+        """
+        Checks for collisions between the player and any obstacles.
+        
+        :return: True if a collision is detected, False otherwise.
+        """
         for obstacle in self.obstacle_manager.obstacles:
             if self.player.get_rect().colliderect(obstacle.get_rect()):
                 return True
