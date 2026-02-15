c4775195a5759c96b105c70393a9cee3284c4ebe
Nazar2903 <2222mnazar2222@gmail.com>
2026-02-15 14:02:01 +0200

    feat: add Obstacle class

---
+++ classes/obstacle.py
@@ -0,0 +1,20 @@
+import pygame
+from classes.interfaces import ICollidable, IUpdatable, IDrawable
+
+class Obstacle(ICollidable, IUpdatable, IDrawable):
+    def __init__(self, x: int, y: int = -50, speed: int = 6):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.image = pygame.image.load("obstacle.png").convert_alpha()
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+
+    def update(self) -> None:
+        self.y += self.speed
+        self.rect.centery = self.y
+
+    def draw(self, screen: pygame.Surface) -> None:
+        screen.blit(self.image, self.rect)
+
+    def get_rect(self) -> pygame.Rect:
+        return self.rect
