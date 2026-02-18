705eab1531fc8beb3c7cf64f5c2f5f1b418f027c
Olga Yezerska <olga_ezerska06@ukr.net>
2026-02-18 19:03:40 +0200

    feat: add class score and methods

---
+++ classes/score.py
@@ -0,0 +1,34 @@
+class Score():
+    """
+    Клас балів гравця
+    """
+    def __init__(self, initial_score=0): 
+        """
+        Ініціалізує об'єкт балів
+        
+        :param initial_score: початкове значення балів
+        """
+        self.score = initial_score
+
+    def increment(self, points=1):
+        """
+        Функція збільшення кількості балів
+        
+        :param points: на скільки збільшується при ітерації
+        """
+        self.score += points
+
+    def reset(self):
+        """
+        Функція обнулення
+        """
+        self.score = 0
+
+    def get_score(self) -> int:
+        """
+        Функція для повернення поточної кількості балів
+        
+        :return: поточна кількість балів
+        :rtype: int
+        """
+        return self.score
