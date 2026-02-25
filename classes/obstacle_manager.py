58a17017304cf42ffd88a2275bc85034f12b9c28
Nazar2903 <2222mnazar2222@gmail.com>
2026-02-25 19:40:21 +0200

    fix: prevent obstacles from overlapping during spawn

--- classes/obstacle_manager.py
+++ classes/obstacle_manager.py
@@ -13,6 +13,10 @@ class ObstacleManager:
         self.scroll_speed = scroll_speed
         self.screen_height = screen_height
         self.obstacles = []
+        
+        # Визначаємо розмір "буферної зони" у пікселях (ширина, висота).
+        # Це значення додається до розміру перешкоди для перевірки вільного місця.
+        self.safe_margin = (80, 200)
 
     def update(self) -> None:
         """
@@ -32,8 +36,23 @@ class ObstacleManager:
             else:
                 obs_type = "normal"
                 
-            obs = Obstacle(x, obstacle_type=obs_type, speed=self.scroll_speed)
-            self.obstacles.append(obs)
+            # 1. Створюємо нову перешкоду, але поки НЕ додаємо її в основний список
+            new_obs = Obstacle(x, obstacle_type=obs_type, speed=self.scroll_speed)
+            
+            # 2. Створюємо збільшений прямокутник (безпечну зону) навколо нової перешкоди
+            # inflate(x, y) розширює прямокутник з центру на задану кількість пікселів
+            safe_zone = new_obs.get_rect().inflate(self.safe_margin[0], self.safe_margin[1])
+            
+            # 3. Перевіряємо, чи перетинається ця зона з існуючими перешкодами
+            safe_to_spawn = True
+            for obs in self.obstacles:
+                if safe_zone.colliderect(obs.get_rect()):
+                    safe_to_spawn = False
+                    break # Якщо знайшли хоча б один перетин - перериваємо цикл
+            
+            # 4. Додаємо перешкоду в гру ТІЛЬКИ якщо навколо неї достатньо місця
+            if safe_to_spawn:
+                self.obstacles.append(new_obs)
 
         # Оновити існуючі перешкоди та видалити ті, що знаходяться поза екраном
         for obs in self.obstacles[:]:
