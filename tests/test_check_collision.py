64d5cc80d960591b18f68d59dc68327db39e7b29
Nazar2903 <2222mnazar2222@gmail.com>
2026-03-20 17:16:51 +0200

    style: fix flake8 warnings in test_check_collision.py

--- tests/test_check_collision.py
+++ tests/test_check_collision.py
@@ -2,14 +2,16 @@ from classes.collision_system import CollisionSystem
 import pytest
 from unittest.mock import MagicMock
 
+
 @pytest.fixture
 def obstacle_manager(obstacle):
     """
-    фікстура для ініціалізації менеджера перешкод 
+    фікстура для ініціалізації менеджера перешкод
     """
     obstacle_manager = MagicMock()
-    obstacle_manager.obstacles = [obstacle] #у масив перешкод передається одна з фікстур 
+    obstacle_manager.obstacles = [obstacle]  # у масив перешкод передається одна з фікстур
     yield obstacle_manager
+
 
 @pytest.mark.collision
 def test_collision(car, obstacle_manager):
@@ -17,4 +19,4 @@ def test_collision(car, obstacle_manager):
     Тест перевірки зіткнення гравця з перешкодою
     """
     collision_system = CollisionSystem(car, obstacle_manager)
-    assert collision_system.check_collisions() == True
+    assert collision_system.check_collisions() is True
