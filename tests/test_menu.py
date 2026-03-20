7643d49614bd295d47c881d00012c57915930eab
Nazar2903 <2222mnazar2222@gmail.com>
2026-03-20 17:24:32 +0200

    style: fix flake8 warnings in test_menu.py

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -3,33 +3,33 @@ import pygame
 from unittest.mock import MagicMock
 from classes.menu import Menu
 
+
 @pytest.fixture
 def menu():
     """
     Фікстура для ініціалізації Меню
     """
-    pygame.init() #для ініціалізації шрифтів всередині меню
+    pygame.init()  # для ініціалізації шрифтів всередині меню
     screen = MagicMock()
     settings = MagicMock()
     menu = Menu(screen, settings)
     yield menu
-    pygame.quit()         
+    pygame.quit()
+
 
 @pytest.mark.menu
 def test_menu_navigate_down(menu):
     """
     Тест перевірки навігації з клавіатури - натиснення клавіші "вниз" спускає логіку на індекс 1
     """
-    input_data = {
-            "quit": False,
-            "enter": False,
-            "escape": False,
-            "up": False,
-            "down": True,
-            "left": False,
-            "right": False,
-            "pause_toggle": False
-        } #словник значень 
+    input_data = {"quit": False,
+                  "enter": False,
+                  "escape": False,
+                  "up": False,
+                  "down": True,
+                  "left": False,
+                  "right": False,
+                  "pause_toggle": False}  # словник значень
     
     menu.handle_input(input_data)
     assert menu.selected_index == 1
