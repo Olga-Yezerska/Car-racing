b64a09d27559b5fb9d8a78a531537def1c72b6f3
Nazar2903 <2222mnazar2222@gmail.com>
2026-03-20 17:18:14 +0200

    style: fix flake8 warnings in test_cycle_complete.py

--- tests/test_cycle_complete.py
+++ tests/test_cycle_complete.py
@@ -1,13 +1,15 @@
 import pytest
 from classes.score import Score
 from classes.score_manager import ScoreManager
- 
+
+
 @pytest.fixture
 def manager():
     """
     Фікстура для ініціалізації менеджер абалів
     """
     return ScoreManager(Score())
+
  
 @pytest.mark.cycle_complete
 def test_cycle_complete(manager):
